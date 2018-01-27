742807efef38addbb1a444f04db93d37419f4611
Molly Jean Bennett <mollyjeanbennett@gmail.com>
2018-01-27 10:37:42 -0800

    updates to GET endpoint test

--- test/test-blog-integration.js
+++ test/test-blog-integration.js
@@ -12,3 +12,65 @@ const { app, runServer, closeServer } = require("../server");
 const { TEST_DATABASE_URL } = require("../config");
 
 chai.use(chaiHttp);
+
+function seedBlogData() {
+  console.info("seeding blog data");
+  const seedData = [];
+
+  for (let i = 0; i < seedData.length; i++) {
+    seedData.push(generateBlogData());
+  }
+  return BlogPost.insertMany(seedData);
+}
+
+function generateBlogData() {
+  return {
+    author: {
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName()
+    },
+    title: faker.lorem.word(),
+    content: faker.lorem.text()
+  };
+}
+
+function tearDownDb() {
+  console.warn("Deleting database");
+  return mongoose.connection.dropDatabase();
+}
+
+describe("Blog API resource", function() {
+  before(function() {
+    return runServer(TEST_DATABASE_URL);
+  });
+
+  beforeEach(function() {
+    return seedBlogData();
+  });
+
+  afterEach(function() {
+    return tearDownDb();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+  describe("GET endpoint", function() {
+    it("should return all existing blog posts", function() {
+      let res;
+      return chai
+        .request(app)
+        .get("/posts")
+        .then(function(_res) {
+          res = _res;
+          expect(res).to.have.status(200);
+          expect(res.body.posts).to.have.length.of.at.least(1);
+          return BlogPost.count();
+        })
+        .then(function(count) {
+          expect(res.body.posts).to.have.length.of(count);
+        });
+    });
+  });
+});
